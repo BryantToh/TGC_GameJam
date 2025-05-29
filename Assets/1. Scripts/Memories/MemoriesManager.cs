861a90353eb6e1147f39e8e52f741fb885d382db
Ev3nts <211208D@mymail.nyp.edu.sg>
2025-05-30 01:50:07 +0800

    making script to handle the showing of memories

---
+++ Assets/1. Scripts/Memories/MemoriesManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MemoriesManager : MonoBehaviour
+{
+    private float fadeDuration = 1.5f;
+    [SerializeField] List<GameObject> memoryPrefabs = new List<GameObject>();
+    private void Start()
+    {
+        for (int i = 0; i < memoryPrefabs.Count; i++)
+        {
+            foreach (Transform child in memoryPrefabs[i].transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+            memoryPrefabs[i].SetActive(false);
+        }
+    }
+    private void Update()
+    {
+        if (LevelManager.Instance.GetCurrLevel() == 2 && LevelManager.Instance.IsLevelCompleted())
+        {
+            CanShowMemoryPanels();
+        }
+    }
+    private void CanShowMemoryPanels()
+    {
+        for (int i = 0; i < memoryPrefabs.Count; i++)
+        {
+            memoryPrefabs[i].SetActive(true);
+            foreach (Transform child in memoryPrefabs[i].transform)
+            {
+                child.gameObject.SetActive(true);
+                Image image = child.gameObject.GetComponentInChildren<Image>();
+                if (image != null)
+                {
+                    StartCoroutine(FadeIn(image));
+                }
+            }
+        }
+    }
+    private IEnumerator FadeIn(Image image)
+    {
+        float elapsed = 0f;
+        Color color = image.color;
+
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            color.a = Mathf.Clamp01(elapsed / fadeDuration);
+            image.color = color;
+            yield return null;
+        }
+
+        color.a = 1f;
+        image.color = color;
+    }
+}
