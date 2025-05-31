ed1bf8c6e1947d5d46c2c4b25d00b44b448f9835
Ev3nts <211208D@mymail.nyp.edu.sg>
2025-05-31 22:22:39 +0800

    ur mother

--- Assets/1. Scripts/Memories/MemoriesManager.cs
+++ Assets/1. Scripts/Memories/MemoriesManager.cs
@@ -21,7 +21,7 @@ public class MemoriesManager : MonoBehaviour
     }
     private void Update()
     {
-        if (LevelManager.Instance.GetCurrLevel() == 2 && LevelManager.Instance.IsLevelCompleted())
+        if (Input.GetKeyDown(KeyCode.P)/*LevelManager.Instance.GetCurrLevel() == 2 && LevelManager.Instance.IsLevelCompleted()*/)
         {
             CanShowMemoryPanels();
         }
