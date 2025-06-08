1c37d512191ca98e1723865c337c8f006b57db8a
AidilGit <72850017+RozeGift@users.noreply.github.com>
2025-06-09 03:21:03 +0800

    - Fixed health issues

--- Assets/1. Scripts/LevelManager/LevelManager.cs
+++ Assets/1. Scripts/LevelManager/LevelManager.cs
@@ -48,6 +48,7 @@ public class LevelManager : MonoBehaviour
             }
             else
             {
+                PlayerStatus.Instance.ResetStatus();
                 StartCoroutine(HandleFinalLevel());
             }
         }
